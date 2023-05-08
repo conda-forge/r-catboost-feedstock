02e354b841213a61f969911ac500c1035aeebf0b
Mervin Fansler <mmfansler@gmail.com>
2023-05-08 16:13:17 -0400

    no tests

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]]; then
   export DISABLE_AUTOBREW=1
-  $R CMD INSTALL --install-tests --build .
+  $R CMD INSTALL --build .
 elif [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
   ## rename to dylib
