64cfd46aeb646339792a9a65887cd32ed3cbc96c
Mervin Fansler <mmfansler@gmail.com>
2023-05-07 22:11:24 -0400

    so to dylib

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,8 +5,8 @@ if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $
   $R CMD INSTALL --build .
 elif [[ $target_platform == osx-64 ]]; then
   export DISABLE_AUTOBREW=1
-  ## provide dylib
-  ln -s inst/libs/libcatboostr.so inst/libs/libcatboostr.dylib
+  ## rename to dylib
+  mv inst/libs/libcatboostr.so inst/libs/libcatboostr.dylib
   $R CMD INSTALL --build --no-staged-install .
 else
   mkdir -p $PREFIX/lib/R/library/catboost
