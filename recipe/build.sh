c155215c0db43e5ee3db1308cfb7f5a9564772ad
akhropov <akhropov@yandex-team.ru>
2023-05-13 03:13:00 +0400

    Enable osx-arm64 platform

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,7 +3,7 @@
 if [[ $target_platform =~ linux.* ]] || [[ $target_platform == win-32 ]] || [[ $target_platform == win-64 ]]; then
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build .
-elif [[ $target_platform == osx-64 ]]; then
+elif [[ $target_platform == osx-64 ]] || [[ $target_platform == osx-arm64 ]]; then
   export DISABLE_AUTOBREW=1
   $R CMD INSTALL --build --no-staged-install .
 else
